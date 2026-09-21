3a7798da2725c5e7ab64a0091b21987c6245c238
markwrom85 <markwrom85@gmail.com>
2026-09-20 20:41:52 -0600

    enable debug mode to add more players

--- Assets/Scripts/Levels/DungeonManager.cs
+++ Assets/Scripts/Levels/DungeonManager.cs
@@ -4,13 +4,19 @@ using UnityEngine.InputSystem;
 
 public class DungeonManager : MonoBehaviour
 {
-    public bool debugMode = false;
+    [SerializeField] private bool debugMode = false;
     [SerializeField] private PlayerInputManager playerInputManager;
     private List<ArduinoJoystickPlayer> players = new List<ArduinoJoystickPlayer>();
+    private bool wasDebugModeEnabled;
+    private int knownPlayerCount;
+
+    public event System.Action DebugModeEnabled;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        wasDebugModeEnabled = debugMode;
+        knownPlayerCount = playerInputManager != null ? playerInputManager.playerCount : 0;
         players.AddRange(FindObjectsByType<ArduinoJoystickPlayer>());
         if (players.Count > 0)
         {
@@ -25,6 +31,9 @@ public class DungeonManager : MonoBehaviour
             playerInputManager.EnableJoining();
             Debug.Log("No ArduinoJoystickPlayers found. Players can join by pressing the Join button.");
         }
+
+        if (debugMode)
+            EnableDebugMode();
     }
 
     private void OnEnable()
@@ -47,9 +56,8 @@ public class DungeonManager : MonoBehaviour
 
     private void OnPlayerJoined(PlayerInput playerInput)
     {
-        ArduinoJoystickPlayer player = playerInput.GetComponent<ArduinoJoystickPlayer>();
-        if (player == null)
-            player = playerInput.GetComponentInChildren<ArduinoJoystickPlayer>();
+        ArduinoJoystickPlayer player = playerInput.transform.root
+            .GetComponentInChildren<ArduinoJoystickPlayer>(true);
 
         if (player == null)
         {
@@ -59,7 +67,9 @@ public class DungeonManager : MonoBehaviour
         }
 
         if (!players.Contains(player)) players.Add(player);
+        knownPlayerCount = playerInputManager.playerCount;
         SetPlayerPlayable(player);
+        Debug.Log("Player " + playerInput.playerIndex + " joined and is now playable.", player);
     }
 
     private static void SetPlayerPlayable(ArduinoJoystickPlayer player)
@@ -70,6 +80,28 @@ public class DungeonManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (debugMode && !wasDebugModeEnabled)
+            EnableDebugMode();
 
+        wasDebugModeEnabled = debugMode;
+
+        if (debugMode && playerInputManager != null
+            && playerInputManager.playerCount != knownPlayerCount)
+        {
+            knownPlayerCount = playerInputManager.playerCount;
+            foreach (ArduinoJoystickPlayer player in FindObjectsByType<ArduinoJoystickPlayer>())
+            {
+                if (!players.Contains(player)) players.Add(player);
+                SetPlayerPlayable(player);
+            }
+        }
+    }
+
+    private void EnableDebugMode()
+    {
+        wasDebugModeEnabled = true;
+        playerInputManager.EnableJoining();
+        DebugModeEnabled?.Invoke();
+        Debug.Log("Debug mode enabled. Additional players can join.", this);
     }
 }
